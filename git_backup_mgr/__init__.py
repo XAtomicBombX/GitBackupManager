28f66d837f9fa9626d664cde65e79404ba1e5447
XFanblueX <3049197701@qq.com>
2022-09-03 12:45:05 +0800

    更改初始化git函数

--- git_backup_mgr/__init__.py
+++ git_backup_mgr/__init__.py
@@ -1,10 +1,21 @@
 from mcdreforged.api.all import *
-from git import Repo
+from git import Repo, InvalidGitRepositoryError
 from git_backup_mgr.config import Configure
 
 repo: Repo
 
 
 def git_init() -> None:
+    """
+    当不存在.git路径时自动init
+    :return:None
+    """
     global repo
-    repo = Repo(Configure.server_path)  # 使用Configure中的服务器目录初始化git
+    try:
+        repo = Repo(Configure.server_path)
+    except InvalidGitRepositoryError:
+        repo = Repo.init(Configure.server_path)
+
+
+def create_backup():
+    pass
