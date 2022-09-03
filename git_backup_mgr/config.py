279b08daa4ce7405ac2facbd45a2373dff1a3d83
XFanblueX <3049197701@qq.com>
2022-09-03 12:19:28 +0800

    update config.py

---
+++ git_backup_mgr/config.py
@@ -0,0 +1,30 @@
+from typing import List, Dict
+
+from mcdreforged.utils.serializer import Serializable
+
+
+class Configure(Serializable):
+    server_path: str = "./server"
+    ignored_files: List[str] = [
+        'session.lock'
+    ]
+    saved_world_keywords: List[str] = [
+        'Saved the game',  # 1.13+
+        'Saved the world',  # 1.12-
+    ]
+
+    remote_backup: bool = False  # 远程备份开关
+    remote_origin: str = 'example.com'  # 远程Git仓库地址
+
+    timed_backup = True  # 自动备份开关
+    backup_overlay = 30  # 备份间隔(分钟)默认为30min
+
+    permissions: Dict[str, int] = {
+        'init': 2,
+        'make': 1,
+        'back': 2,
+        'confirm': 1,
+        'abort': 1,
+        'list': 0,
+        'remote': 3,
+    }
