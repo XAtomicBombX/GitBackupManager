dfa2b48da44d2a083b960189440be4dd9c6e77c3
XFanblueX <3049197701@qq.com>
2022-09-03 20:38:42 +0800

    更改git_init()函数,添加4个新的事件,实现创建备份及部分指令(待测试)

--- git_backup_mgr/__init__.py
+++ git_backup_mgr/__init__.py
@@ -1,10 +1,24 @@
-from time import sleep
-
 from mcdreforged.api.all import *
 from git import Repo, InvalidGitRepositoryError
 from git_backup_mgr.config import Configure
 
 repo: Repo
+git: Repo.git
+remote: Repo.remote
+config: Configure
+CONFIG_FILE_NAME: str = "GitBackupManager.json"
+
+
+class Events:
+    backup_done = LiteralEvent("git_backup_manager.backup_done")
+    backup_trig = LiteralEvent("git_backup_manager.backup_trig")
+    restore_done = LiteralEvent("git_backup_manager.restore_done")
+    restore_trig = LiteralEvent("git_backup_manager.restore_trig")
+
+
+def load_config(server: PluginServerInterface):
+    global config
+    server.load_config_simple(file_name=CONFIG_FILE_NAME, default_config=Configure, target_class=Configure)
 
 
 def git_init() -> None:
@@ -19,29 +33,54 @@ def git_init() -> None:
         print("[GBM]正在初始化.git文件夹...")
         repo = Repo.init(Configure.server_path)
         print("[GBM]初始化完成")
+    global git
+    git = repo.git
+    if config.remote_backup:
+        git.remote('add', 'origin', config.remote_origin)
+        global remote
+        remote = repo.remote()
 
 
-def create_backup() -> None:
-    pass
+@new_thread("GBM Backup Thread")
+def create_backup(server: ServerInterface, comment) -> None:
+    server.say("[GBM]正在备份...")
+    for worlds in config.saves:
+        git.add(worlds)
+    while True:
+        break
+    git.commit('-m', comment)
+    server.say("[GBM]备份完成!")
+    if config.remote_backup:
+        server.say("[GBM]正在上传...")
+        try:
+            git.push('master')
+        except Exception:
+            server.say(f"[GBM]发生错误!错误为:{Exception}")
+            server.say("[GBM]请根据控制台日志排除错误原因!")
+        else:
+            server.say("[GBM]上传完成!")
 
 
-def auto_create_backup(custom_time=0, default_time=1800, state=True) -> None:
-    """git add and git commit"""
-    create_backup()
-    if custom_time == 0:
-        sleep(default_time)
-    else:
-        sleep(custom_time)
-    auto_create_backup()
+"""此自动备份函数已弃用,新自动备份参见timer.py"""
 
 
-def timer(time: int) -> None:
-    """
-    当执行这个函数时，该线程暂停time秒
-    :param time:
-    :return: None
-    """
-    sleep(time)
+# def auto_create_backup(custom_time=0, default_time=1800, state=True) -> None:
+#     """git add and git commit"""
+#     create_backup()
+#     if custom_time == 0:
+#         sleep(default_time)
+#     else:
+#         sleep(custom_time)
+#     auto_create_backup()
+#
+#
+# def timer(time: int) -> None:
+#     """
+#     当执行这个函数时，该线程暂停time秒
+#     :param time:
+#     :return: None
+#     """
+#     sleep(time)
 
 
 def register_command(server: PluginServerInterface) -> None:
@@ -62,12 +101,12 @@ def register_command(server: PluginServerInterface) -> None:
     server.register_command(
         Literal("!!gb").
         then(
-            Literal("init")
+            Literal("init").runs(git_init)
         ).
         then(
             Literal("make").
             then(
-                GreedyText("comment")
+                GreedyText("comment").runs(lambda src, ctx: create_backup(src.get_server(), ctx["comment"]))
             )
         ).
         then(
