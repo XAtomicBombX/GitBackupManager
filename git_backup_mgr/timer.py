488409c8d197433d8500c0127ab0bd5e46d7854f
XFanblueX <3049197701@qq.com>
2022-09-03 20:36:58 +0800

    自动备份timer.py(WIP)

---
+++ git_backup_mgr/timer.py
@@ -0,0 +1,29 @@
+import time
+from threading import Thread
+
+from mcdreforged.plugin.server_interface import PluginServerInterface
+
+from git_backup_mgr import Events
+
+
+class TimedBackup(Thread):
+    def __init__(self, server: PluginServerInterface) -> None:
+        super().__init__()
+        self.daemon = True
+        self.name = "TimedGBM"
+        self.time_last_backup = time.time()
+        self.server = server
+        self.is_enabled = False
+
+    def get_interval(self):
+        from git_backup_mgr.__init__ import config
+        return config.backup_interval * 60
+
+    def auto_backup(self):
+        while True:
+            while True:
+                if time.time() - self.time_last_backup > self.get_interval():
+                    break
+            if self.is_enabled and self.server.is_server_startup():
+                self.server.dispatch_event(Events.backup_trig,())
+
