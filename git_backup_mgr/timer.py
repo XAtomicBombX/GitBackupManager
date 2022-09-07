9244cbc3a2d815766412b8cf41a5df13ee376912
XFanblueX <3049197701@qq.com>
2022-09-07 17:53:28 +0800

    增加备份时执行操作,设置timer状态

--- git_backup_mgr/timer.py
+++ git_backup_mgr/timer.py
@@ -16,17 +16,29 @@ class TimedBackup(Thread):
         self.is_enabled = False
         self.stop_event = Event()
 
+    def set_enabled(self, value):
+        self.is_enabled = value
+        self.reset_timer()
+
     @staticmethod
     def get_interval():
         from git_backup_mgr import config
         return config.backup_interval * 60
+
+    def reset_timer(self):
+        self.time_last_backup = time.time()
 
     def broadcast_next_backup_time(self):
         next_backup_time = time.strftime("%Y/%m/%d %H:%M:%S",
                                          time.localtime(self.time_last_backup + self.get_interval()))
         broadcast_msg(self.server, f"下次自动备份时间:{next_backup_time}")
 
-    def auto_backup(self):
+    def on_backup_created(self):
+        broadcast_msg(self.server, "检测到新增备份,重置计时器")
+        self.reset_timer()
+        self.broadcast_next_backup_time()
+
+    def run(self):
         while True:
             while True:
                 if self.stop_event.wait(1):
